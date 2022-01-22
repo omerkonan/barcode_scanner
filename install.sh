0731c22ab1f1fa6e9664f99bbaa68369177e45af
OmerKonan <konanomer@gmial.com>
2022-01-22 22:42:28 +0300

    install.sh bug fix

--- install.sh
+++ install.sh
@@ -6,8 +6,13 @@ sudo apt-get install libjasper-dev
 sudo apt-get install libqtgui4
 sudo apt-get install libqt4-test
 
-pip3 install -r requirements.txt
-cd /libs/RaspiWiFi/
-chmod +x initial_setup.py
-chmod +x setup_lib.py
-sudo python3 initial_setup.py
+
+pip3 install --upgrade pip setuptools wheel
+pip3 install --upgrade pip
+pip3 install opencv-python==4.5.4.60
+pip3 install -U numpy
+pip3 install pyzbar
+pip3 install pyroute2
+pip3 install azure-iot-device
+pip3 install -r ./libs/mic_hat/requirements.txt
+ 
