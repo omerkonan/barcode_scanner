6aa90d1eb0178a5187e1adb7fa84b51fc4cc1a57
Harmeet Sandhu <harmeet@ctrlaltdel.co>
2022-01-22 15:07:45 +0000

    added barcode token

--- src/barcode_scanning.py
+++ src/barcode_scanning.py
@@ -28,7 +28,7 @@ async def iotsendmsg(msgtoaz):
     await device_client.disconnect()
 
 def getproductinfo(barcode):
-        request = urllib.request.urlopen('https://api.ean-search.org/api?token=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx&op=barcode-lookup&format=json&ean=%s'% (barcode))    
+        request = urllib.request.urlopen('https://api.ean-search.org/api?token=5d0bcaaad5b03b9a3f06a3d170bf0998572765f1c0822075ff&op=barcode-lookup&format=json&ean=%s'% (barcode))    
         response_body = request.read()
         return(response_body.decode('utf-8'))
 
